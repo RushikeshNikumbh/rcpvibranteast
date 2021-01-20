5a78191085fd04ab34e18997af87aa6642886f51
RushikeshNikumbh <RushikeshNikumbh@github.com>
2021-01-21 00:11:37 +0530

    first commit

---
+++ js/redirect.js
@@ -0,0 +1,44 @@
+	/*	document.querySelector('#myBtn1')
+		.addEventListener('click',() => {
+			window.open( 
+              "Get Involved.html", "_blank");
+        });		
+        
+        document.querySelector('#myBtn')
+		.addEventListener('click',() => {
+			window.open( 
+              "rcpve.com", "_blank");
+        });	*/	
+        
+        function joinRotary() {
+            window.open('https://my.rotary.org/en', "_blank")
+          }
+
+
+          function joinRotaract() {
+            window.open('https://www.rotary.org/en/get-involved/rotaract-clubs', "_blank")
+          }
+
+
+          function getStarted() {
+            window.open('Get Involved.html', "_blank")
+          }
+
+          function makeDonation(){
+                window.open('Get Involved.html', "_blank")
+          }
+
+          function fb(){
+                window.open('https://www.facebook.com/rcpvibranteast', "_blank")
+          }
+          function twitter(){
+            window.open('https://www.twitter.com/rcpvibranteast', "_blank")
+            }
+      function yt(){
+            window.open('https://www.youtube.com/channel/UCLrvjcao_Q7odGXqWF0NI5Q/featured', "_blank")
+      }
+      function insta(){
+            window.open('https://www.instagram.com/rcpvibranteast', "_blank")
+      }
+
+
